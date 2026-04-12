7adbe440268ab2c3f6f684092cfe1e4a93c86580
Görkem Hiçyılmaz <130343769+gorkemhc@users.noreply.github.com>
2026-04-12 23:28:34 +0300

    Create baglanti.php

---
+++ assets/baglanti.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "promptla_db";
+$username = "root"; 
+$password = "";     
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Veritabanı bağlantı hatası: " . $e->getMessage());
+}
+?>
